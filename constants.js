6eb07fafe0824408a1fdf06ae05c36db71f4291e
tanaya2903-bornare <63944384+tanaya2903-bornare@users.noreply.github.com>
2023-12-16 18:54:26 +0530

    Delete constants.js

--- constants.js
+++
@@ -1,5 +0,0 @@
-export const ERROR_MESSAGE="Something went wrong";
-export const INSERT_SUCCESS="customer inserted";
-export const STUDENT_NOT_FOUND="customer not found";
-export const UPDATE_SUCCESS="customer updated";
-export const DELETE_SUCCESS="customer deleted";
